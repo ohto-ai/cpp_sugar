f4d1cb23d3b39d7971791166b820c4c627b26fde
Choo <zhu.thatboy@outlook.com>
2024-05-16 18:44:06 +0800

    Add pipe

---
+++ include/suger/pipe.hh
@@ -0,0 +1,144 @@
+#pragma once
+
+#ifndef OHTOAI_SUGER_PIPE_HH
+#define OHTOAI_SUGER_PIPE_HH
+
+#include <ostream>
+#include <algorithm>
+#include <functional>
+
+namespace ohtoai::sugar {
+namespace pipe {
+    // operator|
+    template <typename Arg, typename Fun>
+    constexpr auto operator|(Arg&& arg, Fun&& fun)
+        -> decltype(std::invoke(std::forward<Fun>(fun), std::forward<Arg>(arg))) {
+        return std::invoke(std::forward<Fun>(fun), std::forward<Arg>(arg));
+    }
+
+    // operator| for std::basic_ostream
+    template <typename CharT, typename Traits>
+    constexpr auto operator|(std::basic_ostream<CharT, Traits>& os, std::basic_ostream<CharT, Traits>& (*manip)(std::basic_ostream<CharT, Traits>&))
+        -> decltype(std::invoke(manip, os)) {
+        return std::invoke(manip, os);
+    }
+
+    // filter
+    template <typename F>
+    auto filter(F&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            auto vc = v;
+            //auto it = std::remove_if(std::begin(vc), std::end(vc), std::not_fn(f));
+            auto it = std::remove_if(std::begin(vc), std::end(vc), std::not_fn(f));
+            vc.erase(it, std::end(vc));
+            return vc;
+            };
+    }
+
+    // for_each
+    template <typename F>
+    auto for_each(F&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            auto vc = v;
+            std::for_each(std::begin(vc), std::end(vc), f);
+            return vc;
+            };
+    }
+
+    // transform
+    template <typename Iter, typename F>
+    auto transform(Iter iter, F&& f) {
+        return [iter, f = std::forward<decltype(f)>(f)](auto&& v) {
+            std::transform(std::begin(v), std::end(v), iter, f);
+            return v;
+            };
+    }
+
+    // all_of
+    template <typename F>
+    auto all_of(F&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            return std::all_of(std::begin(v), std::end(v), f);
+            };
+    }
+
+    // any_of
+    template <typename F>
+    auto any_of(F&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            return std::any_of(std::begin(v), std::end(v), f);
+            };
+    }
+
+    // none_of
+    template <typename F>
+    auto none_of(F&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            return std::none_of(std::begin(v), std::end(v), f);
+            };
+    }
+
+    // reverse
+    auto reverse = [](auto&& v) {
+        auto vc = v;
+        std::reverse(std::begin(vc), std::end(vc));
+        return vc;
+        };
+
+    // sort
+    auto sort = [](auto&& v) {
+        auto vc = v;
+        std::sort(std::begin(vc), std::end(vc));
+        return vc;
+        };
+
+    // unique
+    auto unique = [](auto&& v) {
+        auto vc = v;
+        auto it = std::unique(std::begin(vc), std::end(vc));
+        vc.erase(it, std::end(vc));
+        return vc;
+        };
+
+    // copy
+    template <typename Out>
+    auto copy(Out out) {
+        return [out = std::forward<decltype(out)>(out)](auto&& v) {
+            std::copy(std::begin(v), std::end(v), out);
+            return v;
+            };
+    }
+
+    // nop
+    auto nop = [](auto&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& v) {
+            // 如果f含有参数，则调用f(v)，否则调用f()
+            if constexpr (std::is_invocable_v<decltype(f), decltype(v)>) {
+                std::invoke(f, v);
+            }
+            else {
+                std::invoke(f);
+            }
+            return v;
+            };
+        };
+
+    // make_packet
+    template<typename... Ops>
+    auto make_packet(Ops&&... ops) {
+        return [=](auto&& arg) {
+            return (arg | ... | ops);
+            };
+    }
+
+    // tuple_as_args
+    auto tuple_as_args = [](auto&& f) {
+        return [f = std::forward<decltype(f)>(f)](auto&& t) {
+            return std::apply(f, t);
+            };
+        };
+} // namespace pipe
+
+} // namespace ohtoai::sugar
+
+#endif // !OHTOAI_SUGER_PIPE_HH
---
+++ test/pipe_test.cc
@@ -0,0 +1,194 @@
+#include <suger/pipe.hh>
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <vector>
+#include <array>
+
+TEST_CASE("Pipe operator tests", "pipe") {
+    using ohtoai::sugar::pipe::operator|;
+
+    SECTION("Test pipe for basic types") {
+        auto add_one = [](int x) { return x + 1; };
+        auto square = [](int x) { return x * x; };
+
+        REQUIRE((1 | add_one | square) == 4);
+        REQUIRE((2 | add_one | square) == 9);
+        REQUIRE((3 | add_one | square) == 16);
+    }
+
+    SECTION("Test pipe for ostream") {
+        std::ostringstream oss;
+        oss << "Hello, ";
+        oss | std::endl;
+        oss << "world!";
+        REQUIRE(oss.str() == "Hello, \nworld!");
+    }
+}
+
+TEST_CASE("Pipe functor tests", "functor") {
+    using namespace ohtoai::sugar::pipe;
+    SECTION("Test filter") {
+        auto is_even = [](int x) { return x % 2 == 0; };
+
+        std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        auto result = v | filter(is_even);
+        REQUIRE(result == std::vector<int>{2, 4, 6, 8, 10});
+    }
+
+    SECTION("Test for_each") {
+        std::vector<int> v{ 1, 2, 3, 4, 5 };
+        std::vector<int> result;
+        auto print_and_store = [&](int x) { result.push_back(x); };
+
+        v | for_each(print_and_store);
+
+        REQUIRE(result == std::vector<int>{1, 2, 3, 4, 5});
+
+        std::string s = "Hello, world!";
+        auto result_str = s | for_each([](char& c) { c = std::toupper(c); });
+        REQUIRE(result_str == "HELLO, WORLD!");
+        REQUIRE(s == "Hello, world!");
+    }
+
+    SECTION("Test transform") {
+        std::vector<int> v{ 1, 2, 3, 4, 5 };
+        auto square = [](int x) { return x * x; };
+
+        auto result = v | transform(v.begin(), square);
+
+        REQUIRE(result == std::vector<int>{1, 4, 9, 16, 25});
+        REQUIRE(v == std::vector<int>{1, 4, 9, 16, 25});
+    }
+
+    SECTION("Test all_of") {
+        auto is_positive = [](int x) { return x > 0; };
+
+        std::vector<int> v{ 1, 2, 3, 4, 5 };
+        REQUIRE((v | all_of(is_positive)));
+
+        std::vector<int> v2{ 1, -2, 3, -4, 5 };
+        REQUIRE_FALSE((v2 | all_of(is_positive)));
+    }
+
+    SECTION("Test any_of") {
+        auto is_negative = [](int x) { return x < 0; };
+
+        std::vector<int> v{ 1, 2, 3, 4, 5 };
+        REQUIRE_FALSE((v | any_of(is_negative)));
+
+        std::vector<int> v2{ 1, -2, 3, -4, 5 };
+        REQUIRE((v2 | any_of(is_negative)));
+    }
+
+    SECTION("Test none_of") {
+        auto is_negative = [](int x) { return x < 0; };
+
+        std::vector<int> v{ 1, 2, 3, 4, 5 };
+        REQUIRE((v | none_of(is_negative)));
+
+        std::vector<int> v2{ 1, -2, 3, -4, 5 };
+        REQUIRE_FALSE((v2 | none_of(is_negative)));
+    }
+
+    SECTION("Test reverse") {
+        const std::vector<int> v{ 1, 2, 3, 4, 5 };
+        REQUIRE((v | reverse) == std::vector<int>{5, 4, 3, 2, 1});
+
+        const std::string s = "Hello, world!";
+        REQUIRE((s | reverse) == "!dlrow ,olleH");
+    }
+
+    SECTION("Test sort") {
+        std::vector<int> v{ 3, 1, 4, 1, 5, 9, 2, 6 };
+        REQUIRE((v | sort) == std::vector<int>{1, 1, 2, 3, 4, 5, 6, 9});
+    }
+
+    SECTION("Test unique") {
+        std::vector<int> v{ 1, 2, 2, 3, 3, 3, 4, 5, 5 };
+        REQUIRE((v | unique) == std::vector<int>{1, 2, 3, 4, 5});
+    }
+
+    SECTION("Test copy") {
+        std::vector<int> v{ 1, 2, 3 };
+        std::vector<int> dest;
+        v | copy(std::back_inserter(dest));
+        REQUIRE(dest == v);
+    }
+
+    SECTION("Test nop") {
+        std::vector<int> v{ 1, 2, 3 };
+        bool nop_called = false;
+        auto result = v | nop([&nop_called](const auto& v) { nop_called = true; });
+        REQUIRE(result == v);
+    }
+}
+
+TEST_CASE("Pipe tuple call tests", "tuple") {
+    using namespace ohtoai::sugar::pipe;
+    SECTION("Test tuple call") {
+        auto add = [](int x, int y) { return x + y; };
+        auto square = [](int x) { return x * x; };
+
+        auto result = std::make_tuple(1, 2) | tuple_as_args(add) | square;
+        REQUIRE(result == 9);
+    }
+
+    SECTION("Test tuple call with array") {
+        std::array<int, 10> arr{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        auto sum = [](const auto&... args) { return (args + ...); };
+        auto result = arr | tuple_as_args(sum);
+        REQUIRE(result == 55);
+    }
+
+    SECTION("Test tuple call with multiple arguments") {
+        auto add = [](int x, int y, int z) { return x + y + z; };
+        auto square = [](int x) { return x * x; };
+
+        auto result = std::make_tuple(1, 2, 3) | tuple_as_args(add) | square;
+        REQUIRE(result == 36);
+    }
+
+    SECTION("Test tuple call with variadic arguments") {
+        std::stringstream ss;
+        auto print = [&ss](const auto&... args) {
+            (ss << ... << args);
+            };
+
+        std::make_tuple("Hello, ", "world!") | tuple_as_args(print);
+        REQUIRE(ss.str() == "Hello, world!");
+    }
+}
+
+TEST_CASE("Pipe make_packet tests", "make_packet") {
+    using namespace ohtoai::sugar::pipe;
+    SECTION("Test make_packet") {
+        auto add_one = [](int x) { return x + 1; };
+        auto square = [](int x) { return x * x; };
+
+        auto packet = make_packet(add_one, square);
+        REQUIRE((1 | packet) == 4);
+        REQUIRE((2 | packet) == 9);
+        REQUIRE((3 | packet) == 16);
+    }
+
+    SECTION("Test make_packet with multiple operations") {
+        auto add = [](int x, int y) { return x + y; };
+        auto square = [](int x) { return x * x; };
+
+        auto packet = make_packet(tuple_as_args(add), square);
+        REQUIRE((std::make_tuple(1, 2) | packet) == 9);
+        REQUIRE((std::make_tuple(2, 3) | packet) == 25);
+        REQUIRE((std::make_tuple(3, 4) | packet) == 49);
+    }
+
+    SECTION("Test make_packet with variadic arguments") {
+        std::stringstream ss;
+        auto print = [&ss](const auto&... args) {
+            (ss << ... << args);
+            };
+
+        auto packet = make_packet(tuple_as_args(print));
+        packet(std::make_tuple("Hello, ", "world!"));
+        REQUIRE(ss.str() == "Hello, world!");
+    }
+}
