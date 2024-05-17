c460b58e259c1ffda3100f01e5e8cf40713ff550
Choo <zhu.thatboy@outlook.com>
2024-05-18 00:13:51 +0800

    Fix brain_fuck_test.cc to use const char* for brain_fuck output

--- test/brain_fuck_test.cc
+++ test/brain_fuck_test.cc
@@ -5,8 +5,8 @@ TEST_CASE("BrainFuck") {
     using namespace ohtoai::sugar::funny::bf;
     constexpr auto code = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]"
         "<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.>++++++++++.";
-    const char* bf = brain_fuck<brain_fuck_output_size(code)>(code);
+    std::string bf(brain_fuck<brain_fuck_output_size(code)>(code));
     SECTION("Test Hello World") {
-        REQUIRE(std::string(bf) == "Hello World!\n");
+        REQUIRE(bf == "Hello World!\n");
     }
 }
